21d190bce9cd434c6266f687068f425f4f9b4c8b
pfouto <pfouto10@gmail.com>
2021-06-30 17:31:22 +0100

    ...

--- cassandra/deploy/setuptc.sh
+++ cassandra/deploy/setuptc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ -z "$2" ]]; then
-  echo "Usage: setup_delay.sh [nServers] [latency_file]"
+if [[ -z "$3" ]]; then
+  echo "Usage: setup_delay.sh [nServers] [nClients] [latency_file]"
   exit
 fi
 
@@ -10,59 +10,74 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 n_Servers=$1
-latency_file=$2
+n_Clients=$2
+n_Nodes=$((n_Servers + n_Clients))
+latency_file=$3
+
 all_nodes=$(./nodes.sh)
 
-declare -a server_nodes
+default_limit="10000" #10GB
+
+declare -a nodes
 
 idx=0
 for i in $all_nodes; do
-  if [ $idx -eq "$n_Servers" ]; then
+  if [ $idx -eq "$n_Nodes" ]; then
     break
   fi
-  server_nodes+=($i)
+  nodes+=($i)
   idx=$((idx + 1))
 done
 
-if [ $idx -lt "$n_Servers" ]; then
-  echo -e "${RED}Not enough nodes: Got $idx, required ${n_Servers}$NC"
+if [ $idx -lt "$n_Nodes" ]; then
+  echo -e "${RED}Not enough nodes: Got $idx, required ${n_Nodes}$NC"
   exit
 fi
 
-echo -e "${GREEN}Servers: $NC${server_nodes[*]}"
+echo -e "${GREEN}Nodes: $NC${nodes[*]}"
 
 echo Setting up TC...
 
 print_and_exec() {
   node=$1
   cmd=$2
-  echo "-- node -- $cmd"
-  oarsh -n "$node" "$cmd"
+  echo -e "$GREEN -- $node -- $NC$cmd"
+  oarsh -n "$node" "$cmd" 2>&1 | sed "s/^/[$node] /"
 }
 
-for ((i = 0; i < $(($n_Servers)); i++)); do
-  print_and_exec "${server_nodes[$i]}" "sudo-g5k tc qdisc del dev br0 root" &
-done
-wait
-
 i=0
-while read -r line; do
-  echo -e "${RED}${server_nodes[$i]}${NC}"
-
-  print_and_exec "${server_nodes[$i]}" "sudo-g5k tc qdisc add dev br0 root handle 1: htb"
-
+while read -r raw_line; do
+  echo -e "${RED} ${nodes[$i]}----------------------------------------------------------------------${NC}"
+  echo -e "$RED Line: $NC$raw_line "
+  if [[ $raw_line == \#* ]]; then
+    echo "Ignored"
+    continue
+  fi
+  print_and_exec "${nodes[$i]}" "sudo-g5k tc qdisc del dev br0 root; sudo-g5k tc qdisc add dev br0 root handle 1: htb"
   j=0
-  for n in $line; do
+  IFS=', ' read -r -a line <<<"$raw_line"
+
+  upload=${line[0]}
+  download=${line[1]}
+  if [ "${upload}" -eq "0" ]; then
+    upload=$default_limit
+  fi
+  if [ "${download}" -eq "0" ]; then
+    upload=$default_limit
+  fi
+
+  cmd="sudo-g5k tc class add dev br0 parent 1: classid 1:1 htb rate ${upload}mbit"
+  print_and_exec "${nodes[$i]}" "$cmd"
+
+  for n in "${line[@]:2}"; do
     if [ $i -ne $j ]; then
+      target_ip=$(getent hosts "${nodes[$j]}" | awk '{print $1}')
+      echo -e "latency from ${GREEN}${nodes[$i]}${NC} to ${BLUE}${nodes[$j]}${NC} ($target_ip) is ${RED}${n}${NC}"
 
-      target_ip=$(getent hosts "${server_nodes[$j]}" | awk '{print $1}')
-      echo -e "latency from ${GREEN}${server_nodes[$i]}${NC} to ${BLUE}${server_nodes[$j]}${NC} ($target_ip) is ${RED}${n}${NC}"
-
-      cmd="sudo-g5k tc class add dev br0 parent 1: classid 1:$(($j + 1))1 htb rate 1000mbit && \
-          sudo-g5k tc qdisc add dev br0 parent 1:$(($j + 1))1 handle $(($j + 1))10: netem delay ${n}ms $((n/20))ms distribution normal && \
-          sudo-g5k tc filter add dev br0 protocol ip parent 1:0 prio 1 u32 match ip dst $target_ip flowid 1:$(($j + 1))1"
-      print_and_exec "${server_nodes[$i]}" "$cmd"
-
+      cmd1="sudo-g5k tc class add dev br0 parent 1:1 classid 1:1$j htb rate 1mbit ceil 20000mbit && "
+      cmd2="sudo-g5k tc qdisc add dev br0 parent 1:1$j netem delay ${n}ms $((n / 20))ms distribution normal && "
+      cmd3="sudo-g5k tc filter add dev br0 protocol ip parent 1:0 prio 1 u32 match ip dst $target_ip flowid 1:1$j"
+      print_and_exec "${nodes[$i]}" "$cmd1$cmd2$cmd3"
     fi
     j=$((j + 1))
   done
